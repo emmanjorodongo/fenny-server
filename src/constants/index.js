9256f366428fb4ece4dbf81c29ce6e554445017e
emmanjorodongo <102361778+emmanjorodongo@users.noreply.github.com>
2023-12-08 07:51:46 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,4 +6,9 @@ module.exports = {
   SERVER_URL: process.env.SERVER_URL,
   CLIENT_URL: process.env.CLIENT_URL,
   SECRET: process.env.SECRET,
+  POSTGRES_USER: process.env.POSTGRES_USER,
+  POSTGRES_HOST: process.env.POSTGRES_HOST,
+  POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_PORT: process.env.POSTGRES_PORT
 }
