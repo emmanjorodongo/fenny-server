1de4de676c882330905ba78a1c42d0aef5c6f080
emmanjorodongo <102361778+emmanjorodongo@users.noreply.github.com>
2023-12-08 07:52:07 +0300

    Update  database connection environment index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,11 @@
+const { POSTGRES_USER, POSTGRES_HOST, POSTGRES_DATABASE, POSTGRES_PASSWORD, POSTGRES_PORT } = require('../constants')
 const {Pool} =  require('pg')
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'safpro',
-    password: 'superadmin',
-    port: 5433,
+    user: POSTGRES_USER,
+    host: POSTGRES_HOST,
+    database: POSTGRES_DATABASE,
+    password: POSTGRES_PASSWORD,
+    port: POSTGRES_PORT,
 })
 module.exports = {
     query: (text, params) => pool.query (text, params),
